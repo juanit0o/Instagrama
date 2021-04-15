6a1c9ffe4d77fe95755947142bd88258ff3997e4
Francisco Nascimento <ffreiredonascimento1@gmail.com>
2021-04-15 16:05:44 +0100

    PERFIL: Bugs fixed, Likes front fixed

--- ProjetoPSI/Angular/projetoPSI20/src/app/perfil/perfil.component.ts
+++ ProjetoPSI/Angular/projetoPSI20/src/app/perfil/perfil.component.ts
@@ -54,19 +54,60 @@ export class PerfilComponent implements OnInit {
     });
   }
 
-  likeInvoke(id: string) {
-    if(this.liked.includes(id)){
-      window.document.getElementById("like"+id)!.setAttribute('src',"assets/heart.png");
-      const index = this.liked.indexOf(id, 0);
+  likeInvoke(photo: Photo) {
+    if(photo.likes.includes(this.nickname as string)){
+      // Remover like
+
+      // Unlike foto com id passado
+      window.document.getElementById("like"+photo.id)!.setAttribute('src',"assets/heart.png");
+
+      // Remove id da lista de likes
+      const index = this.liked.indexOf(photo.id, 0);
       if (index > -1) {
         this.liked.splice(index, 1);
       }
-    } else {
-      window.document.getElementById("like"+id)!.setAttribute('src',"assets/likebutton.png");
-      this.liked.push(id);
-    }
 
-    //TODO ATUALIZAR BD
+      // Remove id da lista de likes da foto
+      const indexId = photo.likes.indexOf(this.nickname as string, 0);
+      if (indexId > -1) {
+        photo.likes.splice(indexId, 1);
+      }
+
+      // Atualizar número de likes mostrado
+      window.document.getElementById("numberLikes"+photo.id)!.innerHTML = (parseInt(window.document.getElementById("numberLikes"+photo.id)!.innerHTML) - 1).toString();
+
+      // Atualizar BD
+      this.photoService.removeLikeToPhoto(photo.id, this.nickname as string);
+      console.log(photo.likes);
+
+
+    } else {
+      console.log(photo.likes);
+      // Adicionar like
+
+      // Sanitize the list photos.likes
+      // No caso em que a primeira posição seja ""
+      if(photo.likes[0] == "") {
+        photo.likes.splice(0, 1);
+      }
+
+      // Like foto com id passado
+      window.document.getElementById("like"+photo.id)!.setAttribute('src',"assets/likebutton.png");
+
+      // Atualizar lista de fotos curtidas
+      this.liked.push(photo.id);
+
+      // Adicionar cliente corrente a lista de usuários que curtiram a foto
+      photo.likes.push(this.nickname as string);
+
+      // Atualizar número de likes mostrado
+      window.document.getElementById("numberLikes"+photo.id)!.innerHTML = (parseInt(window.document.getElementById("numberLikes"+photo.id)!.innerHTML) + 1).toString();
+
+      // Atualizar BD
+      this.photoService.addLikeToPhoto(photo.id, this.nickname as string);
+
+      console.log(photo.likes);
+    }
 
   }
 
@@ -83,15 +124,6 @@ export class PerfilComponent implements OnInit {
     }
 
     //TODO ATUALIZAR BD
-  }
-
-  /**
-   *
-   * @returns True caso o cliente corrente seja o dono do perfil a ser mostrado
-   */
-  userIsProfileOwner() {
-    console.log(this.auth.getUserDetails()?.nickname);
-    return true ? this.nickname == this.auth.getUserDetails()?.nickname : false;
   }
 
 }
